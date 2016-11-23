dbcb426711bb94d78edc5a5a6e6b55bf9b6a774e
Wim Van Leuven <wim.vanleuven@bigboards.io>
2016-11-23 14:31:09 +0100

    make sure user root and bb folder exist on HDFS

--- hdfs-namenode-wrapper.sh
+++ hdfs-namenode-wrapper.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
-HDFS_FORMAT_MARKER="/opt/hadoop/etc/hadoop/hdfs.format"
+
+FILE_PREFIX="file://"
+HDFS_FORMAT_MARKER="/etc/hadoop/hdfs.format"
+
+HDFS_PATH_NN="$(xpath -q  -e '/configuration/property[name="dfs.namenode.name.dir"]/value/text()' '/etc/hadoop/conf/hdfs-site.xml')"
+HDFS_PATH_NN="${HDFS_PATH_NN#$FILE_PREFIX}"
+
+HDFS_FORMATTED_MARKER=$HDFS_PATH_NN"/current/VERSION"
+
 DAEMON=$1
 
 if [ -e "$HDFS_FORMAT_MARKER" ]; then
-    echo "Found the HDFS format marker. Formatting hdfs before starting";
-    
-    mkdir -p /data/dfs/nn
+    echo "Found the HDFS format marker ... check formatted or not ...";
 
-    /opt/hadoop/bin/hadoop --config /opt/hadoop/etc/hadoop namenode -format -force -nonInteractive
+    if [ ! -f "$HDFS_FORMATTED_MARKER" ]; then
+        echo "Did not find the VERSION formatted marker. Formatting hdfs before starting";
 
-    if [ $? -eq 0 ]; then
+        mkdir -p $HDFS_PATH_NN
+
+        /usr/bin/hdfs --config /etc/hadoop/conf namenode -format -force -nonInteractive
+	    /usr/bin/hdfs --config /etc/hadoop/conf dfs -mkdir -p /user/root
+	    /usr/bin/hdfs --config /etc/hadoop/conf dfs -mkdir -p /user/bb
+
+        if [ $? -eq 0 ]; then
+            rm -rf "$HDFS_FORMAT_MARKER"
+        fi
+    else
         rm -rf "$HDFS_FORMAT_MARKER"
     fi
 fi
 
 if [ ! -e "$HDFS_FORMAT_MARKER" ]; then
-    /opt/hadoop/bin/hdfs --config /opt/hadoop/etc/hadoop namenode
+    /usr/bin/hdfs --config /etc/hadoop/conf namenode
 fi
