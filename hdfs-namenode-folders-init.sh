3679b6501ead602408ec3a65144686a89dd22b5b
Wim Van Leuven <wim.vanleuven@bigboards.io>
2016-11-27 18:43:23 +0100

    wait for name node to start

--- hdfs-namenode-folders-init.sh
+++ hdfs-namenode-folders-init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sleep 10
+/opt/hadoop/bin/hdfs --config /opt/hadoop/etc/hadoop dfsadmin -safemode wait
 /opt/hadoop/bin/hdfs --config /opt/hadoop/etc/hadoop dfs -mkdir -p "/tmp" "/user/root" "/user/bb"
 /opt/hadoop/bin/hdfs --config /opt/hadoop/etc/hadoop dfs -chown -R root:supergroup "/tmp" "/user"
 /opt/hadoop/bin/hdfs --config /opt/hadoop/etc/hadoop dfs -chown -R bb:bigboards "/tmp" "/user/bb"
